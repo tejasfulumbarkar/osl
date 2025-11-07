455400e2e3fa2df90ead635a888491a7d740ae29
Tejas Fulumbarkar <tejasshyamfulumbarkar@gmail.com>
2025-11-07 15:15:11 +0530

    Refactor page replacement logic in optimal.c

--- optimal.c
+++ optimal.c
@@ -1,78 +1,86 @@
 #include<stdio.h>
 int main()
 {
-int frames_number, faults, pages_number, frames[10], pages[30], temp[10], flag1, flag2, flag3, i, j, k, pos, max, miss = 0;
-printf("Enter number of frames: ");
-scanf("%d", & frames_number);
-printf("Enter number of pages: ");
-scanf("%d", &pages_number);
-printf("Enter page reference string: ");
-for(i = 0; i < pages_number; ++i){
-scanf("%d", &pages[i]);
-}
-for(i = 0; i < frames_number; ++i){
-frames[i] = -1;
-}
-for(i = 0; i < pages_number; ++i){
-flag1 = flag2 = 0;
-for(j = 0; j < frames_number; ++j){
-if(frames[j] == pages[i]){
-flag1 = flag2 = 1; break;
-}
-}
+    int frames_number, faults = 0, pages_number, frames[10], pages[30], temp[10], flag1, flag2, flag3, i, j, k, pos, max;
+    printf("Enter number of frames: ");
+    scanf("%d", & frames_number);
+    printf("Enter number of pages: ");
+    scanf("%d", &pages_number);
+    printf("Enter page reference string: ");
+    
+    for(i = 0; i < pages_number; ++i){
+        scanf("%d", &pages[i]);
+    }
 
-//definition of the flag at the starting of the string
-if(flag1 == 0){
-for(j = 0; j < frames_number; ++j){
-if(frames[j] == -1){
-faults++;
-frames[j] = pages[i];
-flag2 = 1;
-break;
-}
-}
-}
-// definition of the flag at the mid position
-if(flag2 == 0){
-flag3 =0;
-for(j = 0; j < frames_number; ++j){
-temp[j] = -1;
-for(k = i + 1; k < pages_number; ++k){
-if(frames[j] == pages[k]){
-temp[j] = k;
-break;
-}
-}
-}
+    for(i = 0; i < frames_number; ++i){
+        frames[i] = -1;
+    }
 
-for(j = 0; j < frames_number; ++j){
-if(temp[j] == -1){
-pos = j;
-flag3 = 1;
-break;
-}
-}
-//definition of flag at the rear position
-if(flag3 ==0){
-max = temp[0];
-pos = 0;
-for(j = 1; j < frames_number; ++j){ if(temp[j] > max){
-max = temp[j];
-pos = j;
-}
-}
-}
-frames[pos] = pages[i];
-faults++;
-//miss++;
-}
+    for(i = 0; i < pages_number; ++i){
+        flag1 = flag2 = 0;
 
-printf("\n");
-for(j = 0; j < frames_number; ++j){
-printf("%d\t", frames[j]);
-}
-}
-printf("\n\nTotal Page miss = %d", faults);
-//printf("\n\nTotal Page miss = %d", miss);
-return 0;
+        //Check if page is already in frame
+        for(j = 0; j < frames_number; ++j){
+            if(frames[j] == pages[i]){
+                flag1 = flag2 = 1;
+                break;
+            }
+        }
+
+        //Empty space case
+        if(flag1 == 0){
+            for(j = 0; j < frames_number; ++j){
+                if(frames[j] == -1){
+                    faults++;
+                    frames[j] = pages[i];
+                    flag2 = 1;
+                    break;
+                }
+            }
+        }
+
+        //Page replacement
+        if(flag2 == 0){
+            flag3 = 0;
+            for(j = 0; j < frames_number; ++j){
+                temp[j] = -1;
+                for(k = i + 1; k < pages_number; ++k){
+                    if(frames[j] == pages[k]){
+                        temp[j] = k;
+                        break;
+                    }
+                }
+            }
+
+            for(j = 0; j < frames_number; ++j){
+                if(temp[j] == -1){
+                    pos = j;
+                    flag3 = 1;
+                    break;
+                }
+            }
+
+            if(flag3 == 0){
+                max = temp[0];
+                pos = 0;
+                for(j = 1; j < frames_number; ++j){
+                    if(temp[j] > max){
+                        max = temp[j];
+                        pos = j;
+                    }
+                }
+            }
+
+            frames[pos] = pages[i];
+            faults++;
+        }
+
+        printf("\n");
+        for(j = 0; j < frames_number; ++j){
+            printf("%d\t", frames[j]);
+        }
+    }
+
+    printf("\n\nTotal Page Faults = %d", faults);
+    return 0;
 }
